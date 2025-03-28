ab7126214bb28975acf7461769dc6ac02e8e890c
AadityaMall <aadityarmall@gmail.com>
2025-03-29 01:26:39 +0530

    Update JwtService: extend token expiration time to 24 hours

--- auth-service/src/main/java/com/flight/booking/auth/service/JwtService.java
+++ auth-service/src/main/java/com/flight/booking/auth/service/JwtService.java
@@ -30,7 +30,7 @@ public class JwtService {
                 .setClaims(claims)
                 .setSubject(email)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis()+1000*60*30))
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*60*24))
                 .signWith(getSignKey()).compact();
     }
 
