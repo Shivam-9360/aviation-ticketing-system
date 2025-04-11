2cedbcb9b5a25e28d8b45e74c9fccb7225785d39
Shivam Kulkarni <shivam.suyash@gmail.com>
2025-04-11 21:39:26 +0530

    Refactor database initialization and update Docker configuration for services

--- sql-init/init.sql
+++ sql-init/init.sql
@@ -1,11 +1,8 @@
--- Create user-service if it doesn't exist
+-- Create databases
 CREATE DATABASE IF NOT EXISTS `user-service`;
-
--- Create airport-service if it doesn't exist
 CREATE DATABASE IF NOT EXISTS `airport-service`;
 
--- Grant privileges
-GRANT ALL PRIVILEGES ON `user-service`.* TO 'user'@'%';
-GRANT ALL PRIVILEGES ON `airport-service`.* TO 'user'@'%';
-
+-- Grant permissions
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
